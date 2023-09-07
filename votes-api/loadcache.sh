1625736faf00d78122556fef8e271bf51e1d8c68
Hung Do <nguyenhung_do@hotmail.com>
2023-09-06 21:21:39 -0400

    Update readme

--- votes-api/loadcache.sh
+++ votes-api/loadcache.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# curl -d '{ "voteId": 1, "pollId": "/poll/1", "voterId": "/voters/1", "voteValue": 1 }' -H "Content-Type: application/json" -X POST http://localhost:1082/votes/1
-# curl -d '{ "voteId": 2, "pollId": "/poll/1", "voterId": "/voters/2", "voteValue": 2 }' -H "Content-Type: application/json" -X POST http://localhost:1082/votes/2
-# curl -d '{ "voteId": 3, "pollId": "/poll/1", "voterId": "/voters/3", "voteValue": 3 }' -H "Content-Type: application/json" -X POST http://localhost:1082/votes/3
-
 curl -d '{ "voteId": 1, "pollId": 1, "voterId": 1, "voteValue": 1 }' -H "Content-Type: application/json" -X POST http://localhost:1082/votes/1
 curl -d '{ "voteId": 2, "pollId": 1, "voterId": 2, "voteValue": 2 }' -H "Content-Type: application/json" -X POST http://localhost:1082/votes/2
 curl -d '{ "voteId": 3, "pollId": 1, "voterId": 3, "voteValue": 3 }' -H "Content-Type: application/json" -X POST http://localhost:1082/votes/3
+
+curl -d '{ "voteId": 4, "pollId": 2, "voterId": 1, "voteValue": 2 }' -H "Content-Type: application/json" -X POST http://localhost:1082/votes/4
+curl -d '{ "voteId": 5, "pollId": 2, "voterId": 2, "voteValue": 3 }' -H "Content-Type: application/json" -X POST http://localhost:1082/votes/5
+
+curl -d '{ "voteId": 6, "pollId": 3, "voterId": 3, "voteValue": 1 }' -H "Content-Type: application/json" -X POST http://localhost:1082/votes/6
